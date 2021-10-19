1f732809880d641b5ada75ac8d0b94d1716cc24d
esipode <chad@chaddyck.com>
2021-10-19 16:35:16 -0600

    Fix route order error

--- server.js
+++ server.js
@@ -22,15 +22,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-    //set static folder
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -42,6 +33,16 @@ connection.once("open", () => {
 
 const router = require("./routes/expenses")
 app.use("/expenses", router);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
