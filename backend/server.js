5b15f584faa86066b20772450e9fc1688ff86353
esipode <chad@chaddyck.com>
2021-10-19 13:55:48 -0600

    Fix error on page load

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "build/index.html"));
+    });
+}
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
 
