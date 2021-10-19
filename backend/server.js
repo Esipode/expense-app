2879d70c8983cfa15a211c066bf0e699e123c457
esipode <chad@chaddyck.com>
2021-10-19 15:26:06 -0600

    Fix get request 404

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "build,", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../build/index.html"));
     });
 }
 
@@ -40,7 +40,7 @@ connection.once("open", () => {
     console.log("Successfully connected to MongoDB!")
 })
 
-const router = require("./backend/routes/expenses")
+const router = require("./routes/expenses")
 app.use("/expenses", router);
 
 app.listen(port, () => {
